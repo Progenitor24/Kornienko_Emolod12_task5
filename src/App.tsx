4abd634ae59753a2cc4f5a3b98fcc77125c69cfa
progenitor <korndavid10@gmail.com>
2025-03-12 18:06:26 +0200

    commit change

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
@@ -25,9 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
